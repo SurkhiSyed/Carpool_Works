07d0ae3da0b8983d15777e8113d421f86ced5b9b
SurkhiSyed <ssurkhi56@gmail.com>
2025-07-04 00:20:48 -0400

    test

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -8,7 +8,7 @@ const Cards = () => {
         <h1 className='text-7xl font-bold text-[#00d59a] text-center'>Maps</h1>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-1 gap-8'>
         <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-[#386e4f80]'>
-                <p className='text-center text-1xl font-bold text-white my-6'>Vibecoding for life</p>
+                <p className='text-center text-1xl font-bold text-white my-6'>Vibes</p>
                 <Map/>
             </div> 
         </div>
